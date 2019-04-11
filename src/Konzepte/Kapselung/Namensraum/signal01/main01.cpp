cc0147347305a2114b74d05a6612ddaf02485852
Thomas Mahr <thomas.mahr@th-nuernberg.de>
2019-04-11 10:52:03 +0200

    Korrektur

--- src/Konzepte/Kapselung/Namensraum/signal01/main01.cpp
+++ src/Konzepte/Kapselung/Namensraum/signal01/main01.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main() {
-	Signal ein;
+	Signal ein;/*§signalEin§*/
 	for(unsigned int i=0; i<3; i++) {
-		ein.anhaengen(1+2*i);
+		ein.anhaengen(1+2*i);/*§SignalErstellen§*/
 	}
 	cout << "Eingang: " << ein.text();
-	Prozessor prozessor;
-	Signal aus = prozessor.verarbeiten(aus);
+	Prozessor prozessor;/*§prozessor§*/
+	Signal aus = prozessor.verarbeiten(ein);/*§signalAus§*/
 	cout << "\nAusgang: " << aus.text();
 }
