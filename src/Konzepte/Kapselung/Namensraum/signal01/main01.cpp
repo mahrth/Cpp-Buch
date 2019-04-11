23713d9753f4098e8db9f61582720b3c4d8dfe1f
Thomas Mahr <thomas.mahr@th-nuernberg.de>
2019-04-11 10:53:28 +0200

    Überflüssige Kommentare entfernt

--- src/Konzepte/Kapselung/Namensraum/signal01/main01.cpp
+++ src/Konzepte/Kapselung/Namensraum/signal01/main01.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main() {
-	Signal ein;/*§signalEin§*/
+	Signal ein;
 	for(unsigned int i=0; i<3; i++) {
-		ein.anhaengen(1+2*i);/*§SignalErstellen§*/
+		ein.anhaengen(1+2*i);
 	}
 	cout << "Eingang: " << ein.text();
-	Prozessor prozessor;/*§prozessor§*/
-	Signal aus = prozessor.verarbeiten(ein);/*§signalAus§*/
+	Prozessor prozessor;
+	Signal aus = prozessor.verarbeiten(ein);
 	cout << "\nAusgang: " << aus.text();
 }
